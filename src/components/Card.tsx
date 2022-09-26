b4d1276581d4ac3c362b0590b4dc573b12cf6360
Kevin Simbana <ka.simbana@outlook.es>
2022-09-26 23:07:44 +0200

    add Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Button from "./Button";
+
+interface Props {
+    title: string,
+    body: string,
+    onClick: () => {}
+}
+
+const styles = {
+    container: {
+        backgroundColor: "#596C68",
+        color: "#E3D9CA",
+        padding: "10px 20px",
+        marginBottom: "20px",
+        borderRadius: "20px",
+        buttonContainer: {
+            display: "flex",
+            justifyContent: "flex-end"
+        }
+    }
+}
+
+const Card = (props: Props) => {
+    return (
+        <div style={styles.container}>
+            <h3>{props.title}</h3>
+            <p>{props.body}</p>
+            <div style={styles.container.buttonContainer}>
+                <Button onClick={props.onClick}>
+                    Eliminar
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
